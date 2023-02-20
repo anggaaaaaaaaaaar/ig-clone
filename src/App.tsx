a95b2159be85509c8d146ff4d687e92eebafdde0
anggarpe <anggarpujianto3@gmail.com>
2023-02-20 14:46:00 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from 'antd';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-primary">
+      <Typography.Paragraph className="text-4xl font-bold">Welcome!!</Typography.Paragraph>
+    </div>
+  );
+}
+
+export default App;
